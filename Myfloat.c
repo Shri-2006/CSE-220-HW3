0eeb5f4c2317d8f8df564cf777ca21730f388115
Shriyans <shriyans.singh@stonybrook.edu>
2026-02-15 16:07:17 -0500

    Edits of comments and final check complete for MyFloat.c

--- Myfloat.c
+++ Myfloat.c
@@ -5,26 +5,24 @@
 float construct_float_sf(char sign_bit, char exponent, unsigned int fraction) {
    unsigned int f = 0;
 
-   /*
-    * Start coding here
-    * Use bitwise operations to construct the IEEE 754 float
-    */
-   //Unsigned to do proper shifting.
-   unsigned int sign_bit_int = sign_bit;
+   //Sign bit conversion and shifter.
+   uint32_t sign_bit_int = sign_bit;
    sign_bit_int=sign_bit_int<<31;
 
-   //unsigned to do proper shifting
-   unsigned int exponent_int = exponent;
+   //Exponenet shifter and conversion
+   uint32_t exponent_int = exponent;
    //Preventing unnecessary bits from interfering. Only care about 8 bits.
    exponent_int=exponent&0b11111111;
    exponent_int=exponent_int<<23;
 
-   //keep only first 23 bits
+   //Remove all unnecesssary bits in fraction
    fraction = fraction&0b11111111111111111111111;
 
    //combination of all three parts
    f= sign_bit_int | exponent_int| fraction;
-   //Debugger tool to figure out error
+eep only first 23 bits
+
+   //Debugger printer to figure out error (figured out exponent was problem from here)
    //printf("sign = %X, exponent = %X, fraction = %X\n", sign_bit_int,exponent_int,fraction);
    
 
