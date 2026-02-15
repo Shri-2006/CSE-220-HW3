3f1c3a87673d7373bd5632f4e40ba3b64251aa5f
Shriyans <shriyans.singh@stonybrook.edu>
2026-02-15 18:52:46 -0500

    Added tests to MyFloat.c

--- test_hw3.c
+++ test_hw3.c
@@ -99,6 +99,63 @@ Test(ConstructFloat, negative_examples) {
    cr_assert_float_eq(f, -1.25, 0.00001);
 }
 
+//Shriyans Tests for MyFloat.c
+/*
+Test(ConstructFloat, positive_examples) {
+   float f = construct_float_sf(0x00, 0x00, 0x0000000); //test if remains 0
+   cr_assert_float_eq(f, 0.0, 0.00001);
+}
+
+Test(ConstructFloat, negative_examples) {
+   float f = construct_float_sf(0x01, 0x00, 0x0000000); //tests if negative 0 works (only sign bit has a one)
+   cr_assert_float_eq(f, -0, 0.00001);
+}
+
+Test(ConstructFloat, positive_examples) {
+   float f = construct_float_sf(0x00, 0x7F, 0x000000);//1 means exponent is properly shifted.
+   cr_assert_float_eq(f, 1, 0.00001);
+}
+
+Test(ConstructFloat, negative_examples) {
+   float f = construct_float_sf(0x01, 0x7F, 0x000000);//-1 means exponent is being properl shifted.
+   cr_assert_float_eq(f, -1, 0.00001);
+}
+
+Test(ConstructFloat, positive_examples) {
+   float f = construct_float_sf(0x00, 0x80, 0x200000);//if 2, then 2 verifcations of positive exponent shift works
+   cr_assert_float_eq(f, 2, 0.00001);
+}
+
+Test(ConstructFloat, negative_examples) {
+   float f = construct_float_sf(0x01, 0x80, 0x200000); //if -2 then 2 verifcation of negative exponent shift
+   cr_assert_float_eq(f, -2, 0.00001);
+}
+Test(ConstructFloat, positive_examples) {
+   float f = construct_float_sf(0x00, 0x7F, 0x400000); //1.5 means decimals work
+   cr_assert_float_eq(f, 1.5, 0.00001);
+}
+
+Test(ConstructFloat, negative_examples) {
+   float f = onstruct_float_sf(0x01, 0x7F, 0x400000); //-1.5 negative decimals also work
+   cr_assert_float_eq(f, -1.5, 0.00001);
+}
+
+Test(ConstructFloat, positive_examples) {
+   float f = construct_float_sf(0x00, 0x91, 0x700000); //Checks large numbers (must be larger than 255439)
+   cr_assert_float_eq(f, 491520, 0.00001);
+}
+
+Test(ConstructFloat, negative_examples) {
+   float f = construct_float_sf(0x01, 0x91, 0x700000); //checks large negative numbers (less than -255439 )
+   cr_assert_float_eq(f, -491520, 0.00001);
+}
+
+
+
+
+
+*/
+
 /* =========================
 * PART 3: Integer Conversion
 * ========================= */
