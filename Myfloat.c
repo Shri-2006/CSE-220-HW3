9dcb22d63d1a5c259b9bb1cad6420b8d877a300b
Shriyans <shriyans.singh@stonybrook.edu>
2026-02-14 22:14:33 -0500

    third draft that is failing myfloat.c

--- Myfloat.c
+++ Myfloat.c
@@ -10,17 +10,17 @@ float construct_float_sf(char sign_bit, char exponent, unsigned int fraction) {
     * Use bitwise operations to construct the IEEE 754 float
     */
    //Unsigned to do proper shifting.
-   uint32_t sign_bit_int = sign_bit;
+   unsigned int sign_bit_int = sign_bit;
    sign_bit_int=sign_bit_int<<31;
 
    //unsigned to do proper shifting
-   uint32_t exponent_int = exponent;
+   unsigned int exponent_int = exponent;
    exponent_int=exponent_int<<23;
 
    //combination of all three parts
    f= sign_bit_int | exponent_int| fraction;
 
-   
+   printf("sign = %X, exponent = %X, fraction = %X, f = %f", sign_bit_int,exponent_int,fraction,f);
    
 
    return *(float *)&f;
